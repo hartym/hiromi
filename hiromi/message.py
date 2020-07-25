a88b58ac3b1ee0ca1305a91a488d3028bcebd5ec
Romain Dorgueil <romain@dorgueil.net>
2020-07-25 11:12:46 +0200

    feat: adds default template capability to messages

--- hiromi/message.py
+++ hiromi/message.py
@@ -1,4 +1,8 @@
+import inspect
+import os
 from typing import Dict, Set
+
+from jinja2 import Template
 
 
 class Message:
@@ -12,6 +16,26 @@ class Message:
 
     def validate(self, parameters):
         return parameters
+
+    default_templates = {}
+
+    def get_default_template(self, medium, *variant):
+        if medium not in self.default_templates:
+            self.default_templates[medium] = {}
+
+        if variant not in self.default_templates[medium]:
+            filename = ".".join(
+                (
+                    os.path.join(os.path.dirname(inspect.getfile(type(self))), "templates", self.name),
+                    medium,
+                    *variant,
+                    "j2",
+                )
+            )
+            with open(filename) as f:
+                self.default_templates[medium][variant] = Template(f.read())
+
+        return self.default_templates[medium][variant]
 
 
 class Envelope:
